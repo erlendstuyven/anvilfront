a24f45bdf958beb97220615d48e68e5960ba8132
developer <dev_groeipakket@kindengezin.be>
2017-03-03 12:15:26 +0100

    stefanj,johans: CBABER-3 add some styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
-import { AppComponent } from './app.component';
-import {ChildAllowanceService} from "./childallowance/child-allowance.service";
-import { ChildAllowanceComponent } from './childallowance/child-allowance.component';
+import {AppComponent} from './app.component';
 import {ChildAllowanceModule} from "./childallowance/childallowance.module";
 import {AppRoutingModule} from "./app-routing.module";
 
@@ -18,8 +17,10 @@ import {AppRoutingModule} from "./app-routing.module";
     FormsModule,
     HttpModule,
     AppRoutingModule,
-    ChildAllowanceModule.forRoot()
+    ChildAllowanceModule.forRoot(),
+    NgbModule.forRoot()
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
