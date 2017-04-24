c34603444529ade31d83e6ebd04a09dcbec9ad86
Erlend Stuyven <erlend.stuyven@cegeka.com>
2017-04-24 20:48:17 +0200

    introduce home page with image

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import {MenuComponent } from './menu/menu.component';
 import {SportHorses} from './items/sport-horses/sport-horses.component';
 import {SaleHorses} from "./items/sale-horses/sale-horses.component";
 import {RestConnectorService} from "./rest-connector.service";
+import {Home} from './home/home.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import {RestConnectorService} from "./rest-connector.service";
     MenuComponent,
     SportHorses,
     SaleHorses,
-    YearMonthInputComponent
+    YearMonthInputComponent,
+    Home
   ],
   imports: [
     BrowserModule,
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,15 +1,20 @@
 import {Routes} from '@angular/router';
 import {SportHorses} from "./items/sport-horses/sport-horses.component";
 import {SaleHorses} from "./items/sale-horses/sale-horses.component";
+import {Home} from "./home/home.component";
 
 export const routes: Routes = [
   {
     path: '',
-    component: SportHorses
+    component: Home
   },
   {
+    path: 'home',
+    component: Home
+
+  }, {
     path: 'sportHorses',
-    component: SportHorses,
+    component: SportHorses
 
   },
   {
